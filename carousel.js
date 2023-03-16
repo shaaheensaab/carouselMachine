51d6a9d3050b444c4964dcd037c87c194cfe4476
shaaheensaab <shaaheensaab@gmail.email>
2023-03-17 01:57:11 +0600

    initial commit

---
+++ carousel.js
@@ -0,0 +1,22 @@
+const images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg'
+]
+
+let imgIndex = 0;
+const imgElement = document.getElementById('slider-img');
+const carousel = setInterval (() => {
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    let imgUrl = images[imgIndex];
+    imgElement.setAttribute('src', imgUrl);
+    imgIndex++;
+}, 2000)
